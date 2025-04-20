ce78c1d6992756c549d52c88796ce0b61bf50d4f
nicolashernandezd2 <n.hernandezd2@uniandes.edu.co>
2025-04-20 16:37:37 -0500

    commit inicial

---
+++ src/app/series/series.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SeriesComponent } from './series.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [SeriesComponent]
+})
+export class SeriesModule { }
